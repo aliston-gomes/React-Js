79b5454417caf97275ef3eb6f946d9262af45371
Aliston <aliston4@icloud.com>
2023-12-12 23:56:58 +0530

    new data

---
+++ React-js/src/App.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import Child from "./Child";
+
+function App() {
+  // const [name, SetName] = useState("");
+  const url = "https://fakestoreapi.com/products";
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    window
+      .fetch(url)
+      .then(data => {
+        return data.json();
+      })
+      .then(data => setProducts(data))
+      .catch(error => console.log(error));
+  }, []);
+  console.log(products);
+  if (products.length < 1) {
+    return <div>{<h1>Loading...</h1>}</div>;
+  }
+
+  return (
+    <>
+      <div>
+        {products.length > 0
+          ? products.map((item, index) => {
+              return <h2 key={item.id}>{item.id}</h2>;
+            })
+          : undefined}
+      </div>
+    </>
+  );
+}
+
+export default App;
