a6623b91949ad04e51f2ba58121adeb04de13a70
Aliston <aliston4@icloud.com>
2023-12-14 02:10:12 +0530

    forms using refs

--- React-js/src/App.jsx
+++ React-js/src/App.jsx
@@ -1,11 +1,14 @@
 import "./App.css";
 import Portal from "./portals/Portal";
+import Refs from "./refs/Refs";
 
 function App() {
   return (
     <>
       {/* //!PORTALS */}
-      <Portal />
+      {/* <Portal /> */}
+      {/* //!REFS */}
+      <Refs />
     </>
   );
 }
